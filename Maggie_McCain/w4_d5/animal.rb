6c5a65ba58bb3ff62a7be8aae62aab9a588f1df8
Maggie McCain <maggie.mccain13@gmail.com>
2016-07-24 17:00:33 +1000

    w4d5 happitails homework complete

---
+++ Maggie_McCain/w4_d5/animal.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Animal
+  attr_accessor :name, :species, :age, :gender, :toys
+
+  def initialize(name, species, age, gender, toys)
+    @name = name
+    @species = species
+    @age = age
+    @gender = gender
+    @toys = [toys]
+  end
+end
+
+=begin
+for $shelter[:animals].find |k, y| @name = name
+=end
