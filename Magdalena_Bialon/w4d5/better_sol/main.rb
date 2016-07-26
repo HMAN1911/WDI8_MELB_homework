f2fd83d379fb0ab8fd197e95b5f1624763fff858
Magdalena Bialon <magdalenabialon@gmail.com>
2016-07-27 07:47:48 +1000

    commented hw - maggies solution

--- Magdalena_Bialon/w4d5/better_sol/main.rb
+++ Magdalena_Bialon/w4d5/better_sol/main.rb
@@ -1,83 +1,87 @@
+# NOT MY SOLUTION - it is Maggie's Mccain
+# I put it here so that it's all in one folder and I can see how nice and simply her is :)
+# I hope it's not a problem
+
+
 require 'pry'
 require './animal'
 require './client'
-#global variable in ruby is $variable_name
+
 $shelter = {
   clients: {},
   animals: {}
 }
 
 
-puts 'WELCOME TO HAPPITAILS'
+puts 'WELCOME TO HAPPITAILS SHELTER'
 
 loop do
   print "Enter: display clients, display animals, create animal, create client, adopt, or put up for adoption to continue..."
   response = gets.chomp
 
   if response == 'create animal'
-    print 'What is the animal\'s name? '
+    print 'Enter animal\'s name: '
     name = gets.chomp
-    print 'What species is this animal? '
+    print 'Enter animal\'s species: '
     species = gets.chomp
-    print 'What is the animal\'s age? '
+    print 'Enter animal\'s age: '
     age = gets.chomp
-    print 'The gender? '
+    print 'Enter gender: '
     gender = gets.chomp
-    print 'What toys does it have? '
+    print 'Enter favourite toys: '
     toys = gets.chomp
     $shelter[:animals][name] = Animal.new(name, species, age, gender, toys)
     puts $shelter[:animals][name].name + " the " + $shelter[:animals][name].species + " has joined the shelter"
 
   elsif response == 'create client'
-    print 'What is your name? '
+    print 'Enter client name: '
     name = gets.chomp
-    print 'How many children do you have? '
+    print 'Enter client\'s number of children: '
     children = gets.chomp
-    print 'What is your age? '
+    print 'Enter client\'s age: '
     age = gets.chomp
-    print 'How many pets do you currently have? '
+    print 'How many pets client already have? '
     existing_pets = gets.chomp
     $shelter[:clients][name] = Client.new(name, children, age, existing_pets)
-    puts "Welcome " + $shelter[:clients][name].name + "! We've got you in the client database."
+    puts "Welcome " + $shelter[:clients][name].name + "! CLIENT in client database."
 
   elsif response == 'adopt'
-    puts 'What is your name? '
+    puts 'Enter Client name: '
     client = gets.chomp
-    puts 'Which pet would you like to adopt? '
+    puts 'Enter animal name for adoption: '
     animal = gets.chomp
     if $shelter[:animals].include? animal
       $shelter[:clients][client].add_pet($shelter[:animals][animal])
       $shelter[:animals].delete(animal)
     else
-      puts 'Sorry there\'s no animal in our database with that name'
+      puts 'Sorry, no animal in animal database'
     end
 
   elsif response == 'put up for adoption'
-    puts 'What is your name? '
+    puts 'Enter Client name: '
     client = gets.chomp
-    puts 'Which pet would you like to put up for adoption? '
+    puts 'Enter animal name to give back '
     animal = gets.chomp
-    binding.pry
+    # binding.pry
     $shelter[:clients][client].remove_pet(animal)
     puts "OH NO!  Sorry " + $shelter[:animals][animal].name
 
   elsif response == 'display animals'
     if $shelter[:animals] == {}
-      puts 'Sorry we don\'t have any animals at this time'
+      puts 'Sorry, animal database is empty'
     else
       puts $shelter[:animals].map{|key| print key[1].name + " is a " + key[1].age + ' year old ' + key[1].species + " who likes to play with " + key[1].toys.to_s + ".  "}
-    #why does above statement require [1]- what is this pointing to?
       puts $shelter[:animals]
     end
 
   elsif response == 'display clients'
     if $shelter[:clients] == {}
-      puts 'Sorry we don\'t have any clients at this time'
+      puts 'Sorry, client database is empty'
     else
       puts $shelter[:clients].map{|key| print key[1].name}
       puts $shelter[:clients]
     end
   else
-    puts 'Sorry we don\'t recognize your request.  Let\'s try again...'
+    puts 'Sorry we don\'t recognize your request.  Please try again...'
   end
 end
