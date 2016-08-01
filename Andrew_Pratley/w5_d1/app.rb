3a401bcad4c3e1da5a75deff47eb4bc4866f84ae
Andrew Pratley <pratley.andrew@gmail.com>
2016-08-01 11:38:43 +1000

    movies 3 etc

--- Andrew_Pratley/w5_d1/app.rb
+++ Andrew_Pratley/w5_d1/app.rb
@@ -3,6 +3,9 @@ require 'sinatra/reloader'
 require 'pry'
 require 'httparty'
 
+require_relative 'db_config'
+require_relative 'models/movie'
+
 # the root - home page
 get '/' do
   erb :index
@@ -11,32 +14,56 @@ end
 get '/list' do
   movie_name = params['movie_name']
   result = HTTParty.get("http://omdbapi.com/?s=#{movie_name}")
-  @titles = result["Search"]
-  erb :list
+  if result["Search"].length == 1
+    redirect "/movie?imdb_id=#{result['Search'][0]['imdbID']}"
+  else
+    @titles = result["Search"]
+    erb :list
+  end
+
 end
 
 get '/movie' do
-  movie_name = params['movie_name']
-  @result = HTTParty.get("http://omdbapi.com/?t=#{movie_name}")
-  @title = result['Title']
-  @year = result['Year']
-  @rated = result['Rated']
-  @released = result['Released']
-  @run_time = result['Runtime']
-  @genre = result['Genre']
-  @director = result['Director']
-  @writer = result['Writer']
-  @actors = result['Actors']
-  @plot = result['Plot']
-  @language = result['Language']
-  @country = result['Country']
-  @awards = result['Awards']
-  @poster = result['Poster']
-  @meta_score = result['Metascore']
-  @imdb_rating = result['imdbRating']
-  @type = result['Type']
-  @res = result['Response']
-  erb :movie
+  imdb_id = params['imdb_id']
+  localMovie = Movie.find_by(imdb_id: imdb_id)
+  if !localMovie
+    result = HTTParty.get("http://omdbapi.com/?i=#{imdb_id}")
+    localMovie = Movie.new
+    localMovie.title = result['Title']
+    localMovie.year = result['Year']
+    localMovie.rated = result['Rated']
+    localMovie.released = result['Released']
+    localMovie.run_time = result['Runtime']
+    localMovie.genre = result['Genre']
+    localMovie.director = result['Director']
+    localMovie.writer = result['Writer']
+    localMovie.actors = result['Actors']
+    localMovie.plot = result['Plot']
+    localMovie.language = result['Language']
+    localMovie.country = result['Country']
+    localMovie.awards = result['Awards']
+    localMovie.image_url = result['Poster']
+    localMovie.imdb_id = result['imdbID']
+    localMovie.imdb_rating = result['imdbRating']
+    localMovie.save
+  end
+
+   @title = localMovie.title
+   @year = localMovie.year
+   @rated = localMovie.rated
+   @released = localMovie.released
+   @run_time = localMovie.run_time
+   @genre = localMovie.genre
+   @director = localMovie.director
+   @writer = localMovie.writer
+   @actors = localMovie.actors
+   @plot = localMovie.plot
+   @language = localMovie.language
+   @country = localMovie.country
+   @awards = localMovie.awards
+   @poster = localMovie.image_url
+   @imdb_rating = localMovie.imdb_rating
+   erb :movie
 end
 
 get '/about' do
