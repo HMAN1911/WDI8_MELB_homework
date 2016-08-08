35db590f4cd62b1ff6271e387addcd5e83357187
Sam West-Sooby <s.west.sooby@gmail.com>
2016-08-09 08:49:00 +1000

    Tuesday homework submission

--- Sam_West_Sooby/w5_d1/movie_app2/main.rb
+++ Sam_West_Sooby/w5_d1/movie_app2/main.rb
@@ -7,16 +7,24 @@ get '/' do
   erb :index
 end
 
-get '/movie' do
-  movie = params['movie'].downcase
-  @result = HTTParty.get("http://www.omdbapi.com/?t=#{ movie }")
-  @title = @result['Title']
-  erb :movies
-end
 
 get '/multiple_results' do
   multiple_results = params['multiple_results']
   @results = HTTParty.get("http://www.omdbapi.com/?s=#{ multiple_results }")
   @result_list = @results['Search']
+  if @results["Search"].length == 1
+    @result = HTTParty.get("http://www.omdbapi.com/?t=#{ multiple_results }")
+    @title = @result['Title']
+    return erb :movies
+  end
   erb :multiple_results
+end
+
+
+
+get '/movies' do
+  multiple_results = params["multiple_results"]
+  @result = HTTParty.get("http://www.omdbapi.com/?t=#{ multiple_results }")
+  @title = @result['Title']
+  erb :movies
 end
