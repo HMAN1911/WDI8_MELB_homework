a63e2c7ffbe298be2225c61f880fe4f0902806fe
Tad Davis <taddavisweb@gmail.com>
2016-08-08 21:44:48 +1000

    w7_d1

---
+++ Tad_Davis/w7_d1/phone.rb
@@ -0,0 +1,34 @@
+class Phone
+
+attr_accessor :number
+
+def initialize(number)
+	
+ if number.start_with?('1') && number.length == 11 
+     
+     @number = number.slice!(1, 11 )
+    
+    elsif number.length == 9 || number.length == 11
+    		return @number = "0000000000"
+    
+   
+    else
+      @number = number.gsub(/[^a-z0-9]/i, '')
+    end
+
+    return @number
+
+  end
+
+def area_code
+    return number[0..2]
+end
+
+
+def pretty_print
+
+  return number 
+
+end
+
+end #end of class
