2e9c51ecb105735e3a838a816ba71ba0f1af3b8d
Denis Ng <denisng89@gmail.com>
2016-08-29 11:07:48 +1000

    js functions

---
+++ Denis_Ng/w9/js_functions/functions.js
@@ -0,0 +1,83 @@
+console.log("it works");
+
+// Question 1
+function maxOfTwoNumbers(a,b) {
+  if (a > b) {
+    return a
+  }
+  else {
+    return b
+  }
+}
+
+
+// Question 2
+function maxOfThree(a,b,c) {
+  if (a > b && a > c) {
+    return a
+  }
+  else if (b > a && b > c) {
+    return b
+  }
+  else if (c > a && c > b) {
+    return c
+  }
+}
+
+// Question 3
+function isCharacterAVowel(string) {
+ if (string.length == 1) {
+   and is ["a","e","i","o","u"]
+ } return true
+ else {
+   return false
+ }
+}
+
+// Question 4
+function sumArray(a,b) {
+  return a + b;
+  }
+  console.log(sum)
+}
+
+
+// Question 4
+function multiplyArray(a,b) {
+  return a * b;
+}
+
+
+// Question 5
+var numberOfArguments = function(){
+  return arguments.length
+}
+
+
+
+// Question 6
+var reverseString = function (string){
+  return string.split("").reverse().join("");
+};
+
+
+// Question 7
+function findLongestWord () {
+
+}
+
+
+// Question 8
+function filterLongWords () {
+
+}
+
+
+// Bonus 1
+//??????
+
+
+// Bonus 2
+function charactersOccurencesCount() {
+
+}
